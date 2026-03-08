0c4bcfd3326b7e63754b938d9a649cff8ffbd709
Nabelaldwese <Nbelaldwese1@gmail.com>
2026-03-08 08:00:59 +0000

    fix

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const taskBlock = true;
+    const taskBlock2 = false;
   }
 
   return [task, task2];
